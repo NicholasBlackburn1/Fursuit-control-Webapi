96a6aae8c32fae35eed9180ae3b6dc664621c5c1
Nick Blackburn <nickblackburn02@gmail.com>
2022-08-20 22:13:24 -0400

    setup base project

--- fursuitwebapi/__main__.py
+++ fursuitwebapi/__main__.py
@@ -2,11 +2,26 @@
 main class
 """
 
-from utils import consolelog
+import pathlib
+from utils import consolelog, Consts
+import app
+
 
 # main function
 def main():
     consolelog.info("starting apiserever...")
+
+    app = create_app()
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "sqlite:///" + str(pathlib.Path().absolute()) + "/"+ Consts.folderbase + "avatar.db"
+    )
+
+    
+    
+
+    app.run(threaded=True, debug=True, host=Consts.url, port=2000)
+
+
 
 
 
--- fursuitwebapi/utils/Consts.py
+++ fursuitwebapi/utils/Consts.py
@@ -1,2 +1,6 @@
 base = "/fursuitcontrol/api"
 apibase = base + "/v1.0/"
+
+# folder and api
+folderbase = "FursuitControl/"
+url = "0.0.0.0"
