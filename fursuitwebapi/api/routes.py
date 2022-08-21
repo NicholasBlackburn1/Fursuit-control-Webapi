978eef2bed499c6119c737fe26c67c8cbea595b1
Nick Blackburn <nickblackburn02@gmail.com>
2022-08-20 22:35:53 -0400

    got base app running time to set up dev  and serting basic endpoints

--- fursuitwebapi/api/routes.py
+++ fursuitwebapi/api/routes.py
@@ -11,11 +11,66 @@ from flask import (
     send_from_directory,
     send_file,
 )
+from fursuitwebapi.utils import Consts
 
 apibp = Blueprint("apibp", __name__)
 
-
-@apibp.route('/')
+# main rounter
+@apibp.route(Consts.apibase)
 def index():
     return "hellow world~"
+
+
+
+
+"""
+Led control 
+"""
+
+# reports status of led lights
+@apibp.route(Consts.ledcontrol+"status")
+def ledstatus():
+    pass
+
+
+
+
+
+"""
+Fan Control 
+"""
+
+#reports fan status
+@apibp.route(Consts.fancontrol+"status")
+def fanstatus():
+    pass
+
+
+
+
+
+
+"""
+temp monitoring
+"""
+
+#reports tempature status status
+@apibp.route(Consts.tempcontrol+"status")
+def tempstatus():
+    pass
+
+
+
+
+
+
+"""
+voice modulation control 
+"""
+
+#reports tempature status status
+@apibp.route(Consts.voicecontrol+"status")
+def voicetatus():
+    pass
+
 
--- fursuitwebapi/utils/Consts.py
+++ fursuitwebapi/utils/Consts.py
@@ -1,6 +1,16 @@
+
+"""
+endpoints
+"""
+
 base = "/fursuitcontrol/api"
 apibase = base + "/v1.0/"
 
+#endpoints
+ledcontrol = apibase + "led/"
+fancontrol = apibase + "fan/"
+voicecontrol =  apibase + "voice/"
+tempcontrol = apibase + "temp/"
 # folder and api
 folderbase = "FursuitControl/"
 url = "0.0.0.0"
